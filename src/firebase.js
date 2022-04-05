e0f2524f0d0246016948d64b6f6e2cf30aba21fc
Hugo BOGDANOVIC <hugobogdanovic@free.fr>
2022-04-05 12:12:43 +0200

    connection a firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+const firebaseApp = require("firebase/app")
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAxN4qfLA5Tuiv3XyPeEUuyI3Kbt6It8I0",
+  authDomain: "studifronttrain.firebaseapp.com",
+  projectId: "studifronttrain",
+  storageBucket: "studifronttrain.appspot.com",
+  messagingSenderId: "733405990397",
+  appId: "1:733405990397:web:0a4dac8ace432871549d94"
+};
+
+// Initialize Firebase
+const app = firebaseApp.initializeApp(firebaseConfig);
+
